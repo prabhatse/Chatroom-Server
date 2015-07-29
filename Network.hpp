9be90259208b975f43616b4f5c59a930ec3a0708
james <jameszheng66@gmail.com>
2015-07-28 21:35:06 -0700

    Added comments to functions

--- Network.hpp
+++ Network.hpp
@@ -2,8 +2,8 @@
 #define NETWORK_HPP
 
 #include "Poller.hpp"
-#include "ChatRoom.hpp"
 #include "MessageHandler.hpp"
+#include "ChatRoom.hpp"
 #include <fcntl.h>
 #include <unistd.h>
 #include <netinet/in.h>
@@ -15,22 +15,30 @@
 #include <fstream>
 #include <thread>
 
-class CChatRoom;
 class CNetwork: public CPoller, public CMessageHandler{
 private:
     static CNetwork* network;
     int _sockfd, _portno;
     sockaddr_in _serv_addr;
     CNetwork(int portno);
+    /* a vector of CChatRooms but only create upon 
+     * clients request
+    */
     CChatRoom _rooms[MAX_ROOMS];
     int _numRooms = 0;
+    /* users are added when clients authenticate with their username
+     * users will be removed from network but added to chatroom when
+     * they join a room 
+     */
     std::vector<std::string> _users;
     bool _infoModified = false;
 
 public: 
     static CNetwork& getNetwork(int portno=49999);
     bool acceptConnection();
+    /* set up and wait for any messages */
     bool readPoll(int);
+    /* respond to any messages */
     void start();
     void addRoom(int, std::string);
     bool addUser(std::string);
@@ -39,6 +47,7 @@ public:
     *** Write packets****
     **********************/
     bool processMessage(int, int, std::string&);
+    /* Write to client, are all responses */
     void Write_ClientAuth(int, std::string&);
     void Write_GetAvailableRooms(int, std::string&);
     void Write_GetRoomStatus(int, std::string&);
